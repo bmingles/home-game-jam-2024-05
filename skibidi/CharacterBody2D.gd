56e05928b0ba080281c16598bfa5e61aa966c6ce
Brian Ingles <brian@emeraldwalk.com>
2024-05-18 12:49:26 -0500

    player keys

--- skibidi/CharacterBody2D.gd
+++ skibidi/CharacterBody2D.gd
@@ -19,7 +19,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("player2_left", "player2_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
